b45266dcfd48e413b9fc3da95eb9f19dc7c25c03
David L. Inman <davidlevere@gmail.com>
2022-12-03 00:26:40 -0500

    day 3:1 input

--- src/aoc_2022/inputs/input.rs
+++ src/aoc_2022/inputs/input.rs
@@ -1,3 +1,4 @@
 
 pub static DAY_1_1 : &'static str = include_str!("day_1_1.text");
 pub static DAY_2_1 : &'static str = include_str!("day_2_1.txt");
+pub static DAY_3_1 : &'static str = include_str!("day_3_1.txt");
