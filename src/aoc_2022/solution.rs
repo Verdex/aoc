eb86e23be1b6cb9354c704f7d7d476499440e339
David L. Inman <davidlevere@gmail.com>
2022-12-02 21:17:03 -0500

    solution for day 2:1

--- src/aoc_2022/solution.rs
+++ src/aoc_2022/solution.rs
@@ -1,6 +1,8 @@
 
 use super::inputs::input::DAY_1_1;
+use super::inputs::input::DAY_2_1;
 use super::parsing;
+use super::data::*;
 
 pub fn solve_day_1_2() {
 
@@ -17,5 +19,34 @@ pub fn solve_day_1_2() {
 }
 
 pub fn solve_day_2_1() {
-    
+    use RPS::*;
+
+    fn selection_score(choice : &RPS) -> u64 {
+        match choice {
+            Rock => 1,
+            Paper => 2,
+            Scissors => 3,
+        }
+    }
+
+    fn status_score(opponent : &RPS, you : &RPS) -> u64 {
+        match (opponent, you) {
+            (Rock, Paper) => 6,
+            (Paper, Scissors) => 6,
+            (Scissors, Rock) => 6,
+
+            (Paper, Rock) => 0,
+            (Scissors, Paper) => 0,
+            (Rock, Scissors) => 0,
+
+            _ => 3,
+        }
+    }
+
+    let guide = parsing::rps::parse_rps_list(DAY_2_1);
+    let result : u64 = guide.into_iter()
+                      .map(|(opponent, you)| selection_score(&you) + status_score(&opponent, &you))
+                      .sum();
+
+    println!("2022 day 2:1 = {}", result);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ mod aoc_2015;
 mod aoc_2022;
 
 fn main() {
-    crate::aoc_2015::solution::solve_day_1_1();
-    crate::aoc_2015::solution::solve_day_1_2();
+    //crate::aoc_2015::solution::solve_day_1_1();
+    //crate::aoc_2015::solution::solve_day_1_2();
 
-    crate::aoc_2022::solution::solve_day_1_2();
+    //crate::aoc_2022::solution::solve_day_1_2();
+    crate::aoc_2022::solution::solve_day_2_1();
 }
