9d84d7488fe4cd146fa1003b4d8a4cbf523a1c88
David L. Inman <davidlevere@gmail.com>
2022-12-03 19:58:57 -0500

    2015 day 2:1 input; dimension parser

--- src/aoc_2015/parsing/parser.rs
+++ src/aoc_2015/parsing/parser.rs
@@ -11,6 +11,52 @@ pub fn parse_elevator_instructions( input : &str ) -> Vec<ElevatorInstruction> {
     output
 }
 
+pub fn parse_dimensions( input : &str ) -> Vec<(u32, u32, u32)> {
+    let mut i = input.char_indices();
+    let output = dimensions(&mut i).unwrap();
+    if i.count() != 0 { panic!("all input not consumed"); }
+    output
+}
+
+group!(dimensions: char => Vec<(u32, u32, u32)> = |input| {
+    pred!(x: char => () = |x| x == 'x' => { () });
+    seq!(dim: char => (u32, u32, u32) = n1 <= number, x, n2 <= number, x, n3 <= number, {
+        (n1, n2, n3)
+    });
+
+    seq!(dim_end: char => (u32, u32, u32) = d <= dim, end_line, { d });
+    seq!(dims: char => Vec<(u32, u32, u32)> = ds <= * dim_end, md <= ? dim, {
+        let mut ds = ds;
+        match md {
+            Some(d) => { ds.push(d); ds },
+            None => ds
+        }
+    });
+
+    dims(input)
+});
+
+group!(number: char => u32 = |input| {
+    pred!(digit: char = |x| x.is_numeric());
+    seq!(digits: char => u32 = d <= digit, ds <= * digit, { 
+        let mut ds = ds;
+        ds.insert(0, d);
+        ds.iter().collect::<String>().parse::<u32>().expect("already parsed as valid number")
+    });
+
+    digits(input)
+});
+
+group!(end_line: char => () = |input| {
+    pred!(cr: char => () = |x| x == '\r' => { () });
+    pred!(lf: char => () = |x| x == '\n' => { () });
+    seq!(cr_lf: char => () = cr, lf, { () });
+
+    alt!(main: char => () = cr_lf | lf | cr);
+
+    main(input)
+});
+
 group!(ei: char => Vec<ElevatorInstruction> = |input| {
     pred!(up: char => ElevatorInstruction = |x| x == '(' => { ElevatorInstruction::Up });
     pred!(down: char => ElevatorInstruction = |x| x == ')' => { ElevatorInstruction::Down });
