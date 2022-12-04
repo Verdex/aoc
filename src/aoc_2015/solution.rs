c0bda8adc3d1d37ff23a69b46b37361f371e3dbd
David L. Inman <davidlevere@gmail.com>
2022-12-03 22:51:06 -0500

    2015 day 3:2

--- src/aoc_2015/solution.rs
+++ src/aoc_2015/solution.rs
@@ -45,7 +45,7 @@ pub fn solve_day_2_1() {
 
     let result = dims.into_iter().map(|x| req_paper(x)).sum::<u32>();
 
-    println!("2015 day 2:1 {}", result);
+    println!("2015 day 2:1 = {}", result);
 }
 
 pub fn solve_day_2_2() { 
@@ -65,5 +65,64 @@ pub fn solve_day_2_2() {
 
     let result = dims.into_iter().map(|x| req_bow(x)).sum::<u32>();
 
-    println!("2015 day 2:2 {}", result);
+    println!("2015 day 2:2 = {}", result);
+}
+
+pub fn solve_day_3_1() {
+    let input = DAY_3_1.chars();
+
+    let mut x = 0;
+    let mut y = 0;
+
+    let mut v = vec![(0, 0)];
+
+    for dir in input {
+        match dir {
+            '^' => { y+=1; v.push((x,y)); },
+            '<' => { x-=1; v.push((x,y)); },
+            '>' => { x+=1; v.push((x,y)); },
+            'v' => { y-=1; v.push((x,y)); },
+            _ => unreachable!(),
+        }
+    }
+
+    v.sort();
+    v.dedup();
+
+    println!("2015 day 3:1 = {}", v.len());
+}
+
+pub fn solve_day_3_2() {
+    let input = DAY_3_1.chars();
+
+    let mut x1 = 0;
+    let mut y1 = 0;
+
+    let mut x2 = 0;
+    let mut y2 = 0;
+
+    let mut v = vec![(0, 0)];
+
+    let mut b = false;
+
+    for dir in input {
+        match (dir, b) {
+            ('^', true) => { y1+=1; v.push((x1,y1)); },
+            ('<', true) => { x1-=1; v.push((x1,y1)); },
+            ('>', true) => { x1+=1; v.push((x1,y1)); },
+            ('v', true) => { y1-=1; v.push((x1,y1)); },
+
+            ('^', false) => { y2+=1; v.push((x2,y2)); },
+            ('<', false) => { x2-=1; v.push((x2,y2)); },
+            ('>', false) => { x2+=1; v.push((x2,y2)); },
+            ('v', false) => { y2-=1; v.push((x2,y2)); },
+            _ => unreachable!(),
+        }
+        b = !b;
+    }
+
+    v.sort();
+    v.dedup();
+
+    println!("2015 day 3:2 = {}", v.len());
 }
