c4daec392eca961fd7d95e4c6dd75eecf674472f
David L. Inman <davidlevere@gmail.com>
2022-12-04 09:26:53 -0500

    inc prog

---
+++ src/aoc_2022/solution/day_4.rs
@@ -0,0 +1,4 @@
+
+pub fn solve_1() {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,12 @@ fn main() {
     //crate::aoc_2015::solution::solve_day_2_1();
     //crate::aoc_2015::solution::solve_day_2_2();
     //crate::aoc_2015::solution::solve_day_3_1();
-    crate::aoc_2015::solution::solve_day_3_2();
+    //crate::aoc_2015::solution::solve_day_3_2();
 
     //crate::aoc_2022::solution::day_1::solve_2();
     //crate::aoc_2022::solution::day_2::solve_1();
     //crate::aoc_2022::solution::day_2::solve_2();
     //crate::aoc_2022::solution::day_3::solve_1();
     //crate::aoc_2022::solution::day_3::solve_2();
+    crate::aoc_2022::solution::day_4::solve_1();
 }
