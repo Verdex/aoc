82a3ab02b518bcb9b08fb1b9f92d9ca9d69d4c3c
David L. Inman <davidlevere@gmail.com>
2022-12-05 23:25:12 -0500

    solution file

---
+++ src/aoc_2022/solution/day_5.rs
@@ -0,0 +1,10 @@
+
+use super::super::parsing::crane::*;
+use super::super::inputs::input::*;
+
+pub fn solve_1() {
+    let input = DAY_5_1;
+    let result = parse_crane(input);
+
+    println!("2022 day 5:1 = {:?}", result);
+}
