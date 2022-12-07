bf858a79a40a54cd35e83b3ea6180950799849fb
David L. Inman <davidlevere@gmail.com>
2022-12-06 19:08:47 -0500

    linearizable for list and input for day 6

--- src/aoc_2022/inputs/input.rs
+++ src/aoc_2022/inputs/input.rs
@@ -4,3 +4,4 @@ pub static DAY_2_1 : &'static str = include_str!("day_2_1.txt");
 pub static DAY_3_1 : &'static str = include_str!("day_3_1.txt");
 pub static DAY_4_1 : &'static str = include_str!("day_4_1.txt");
 pub static DAY_5_1 : &'static str = include_str!("day_5_1.txt");
+pub static DAY_6_1 : &'static str = include_str!("day_6_1.txt");
--- src/aoc_2022/solution/day_6.rs
+++ src/aoc_2022/solution/day_6.rs
@@ -1,4 +1,21 @@
 
+use bending::*;
+use denest::*;
+
+enum List<T> {
+    Cons(T, Box<List<T>>),
+    Nil,
+}
+
+impl<'a, T> Linearizable<'a> for List<T> {
+    fn l_next(&'a self) -> Vec<&'a Self> {
+        match self {
+            List::Cons(_, rest) => vec![rest],
+            List::Nil => vec![],
+        }
+    }
+}
+
 pub fn solve_1() {
 
 }
