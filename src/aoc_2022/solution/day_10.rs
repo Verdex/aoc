f20bab71e985b813705b97c1d3e92518aab3ea83
David L. Inman <davidlevere@gmail.com>
2022-12-10 23:40:19 -0500

    inc prog

--- src/aoc_2022/solution/day_10.rs
+++ src/aoc_2022/solution/day_10.rs
@@ -34,11 +34,8 @@ pub fn solve_1() {
 
 #[allow(dead_code)]
 pub fn solve_2() {
-    //let input = EXAMPLE;
     let input = DAY_10_1;
     let asm = parse_asm(input);
-
-    let targets = [20, 60, 100, 140, 180, 220];
 
     let mut cycle = 0;
     let mut x = 1;
@@ -53,7 +50,7 @@ pub fn solve_2() {
             },
             Asm::Noop => { cycle += 1; vec![(cycle, x)] },
         }
-    });//.collect::<Vec<_>>();
+    });
 
     let sprite_positions = states.map(|(cycle, pos)| ((cycle - 1) % 40, vec![pos -1, pos, pos +1])).collect::<Vec<_>>();
     let pos_lit = sprite_positions.into_iter().map(|(pos, sprite)| sprite.iter().any(|s| *s == pos))
@@ -73,150 +70,3 @@ pub fn solve_2() {
         println!("{}", line.iter().collect::<String>());
     }
 }
-
-static EXAMPLE : &str = "addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop";
